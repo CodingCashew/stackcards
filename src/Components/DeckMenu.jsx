12407179e0f5744cb5c18f0a7d25dfcccf08a76f
Liz <Liz5112@users.noreply.github.com>
2022-07-14 23:23:27 -0500

    Create Dropdown Menu for decks, manage state with hooks, render data from the server

--- src/Components/DeckMenu.jsx
+++ src/Components/DeckMenu.jsx
@@ -1,4 +1,4 @@
-import { React, useState } from 'react';
+import { React, useState, useEffect } from 'react';
 import { Container, Text } from '@chakra-ui/react';
 import { ChevronDownIcon } from '@chakra-ui/icons'
 import {
@@ -6,7 +6,8 @@ import {
   MenuButton,
   MenuList,
   MenuItem,
-  Button
+  Button,
+  List
 } from '@chakra-ui/react'
 
 const hardcodedDecks = ['Spanish', 'Chinese', 'Arabic', 'French', 'Persian', 'Swedish', 'Turkish']
@@ -14,25 +15,56 @@ const hardcodedDecks = ['Spanish', 'Chinese', 'Arabic', 'French', 'Persian', 'Sw
 
 function DeckMenu() {
   const [currentDeck, setCurrentDeck] = useState('Deck')
-
-
+  const [cards, setCards] = useState({front: 'almond', back: 'almendra'});
   const handleChangeDeck = (e) => {
     setCurrentDeck(e.target.value)
+    getCards();
   }
 
+  const getCards = async () => {
+    // fetch(`/get${{currentDeck}}`, {
+    fetch('/spanish')
+      .then((res) => res.json())
+      .then((cards) => {
+        console.log('front of first card:', cards[0].front)
+        setCards({front: cards[0].front, back: cards[0].back});
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getCards();
+  }, [currentDeck]);
+
+
+
+
   return (
-    <Container>
+    <Container maxW='3xl'>
       <Menu>
         <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
           {currentDeck}
         </MenuButton>
         <MenuList>
           {hardcodedDecks.map((deck, i) =>
-            <MenuItem key={i} onClick={handleChangeDeck} value={deck}>{deck}</MenuItem>
+            <MenuItem key={i} onClick={handleChangeDeck} onChange={getCards} value={deck}>{deck}</MenuItem>
           )}
         </MenuList>
       </Menu>
       <Text>Your current deck is {currentDeck}</Text>
+      <Button onClick={getCards} bgColor="pink1" color="white">Get Cards</Button>
+      <Container  m={5} >
+        {/* <List> */}
+          {/* <Text>{cards[0].front}</Text> */}
+          <Text fontSize='4xl' color="yellow1">{cards.front}</Text>
+          </Container>
+          <Container m={5} >
+          <Text fontSize='4xl' color="pink2">{cards.back}</Text>
+          {/* {cards.map((card, i) =>
+            <ListItem key={i}>{card}</ListItem>
+          )} */}
+        {/* </List> */}
+      </Container>
       </Container>
   )
 }
