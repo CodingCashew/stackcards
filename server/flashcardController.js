cbfe898ab0702ba51049c84c7af4673265e845e4
Liz <Liz5112@users.noreply.github.com>
2022-07-15 16:52:29 -0500

    modify controller SQL request to apply  params for the requested table

--- server/flashcardController.js
+++ server/flashcardController.js
@@ -2,9 +2,10 @@ const db = require('./FlashcardModel.js');
 
 const flashcardController = {};
 
-// send all coffees in the database to the Shop Page Component on page render
-flashcardController.getSpanish = (req, res, next) => {
-  const queryString = 'SELECT * FROM Spanish;';
+// send all cards of the specified deck from the database to the front end
+flashcardController.getCards = (req, res, next) => {
+  const { currentDeck } = req.params;
+  const queryString = `SELECT * FROM ${currentDeck};`;
 
   db.query(queryString)
     .then((data) => {
@@ -12,9 +13,43 @@ flashcardController.getSpanish = (req, res, next) => {
       return next();
     })
     .catch((err) => next({
-      log: `Error in coffeeController.getSpanish: ${err}`,
-      message: { err: 'Error getting Spanish cards' }
+      log: `Error in coffeeController.getCards: ${err}`,
+      message: { err: 'Error getting Cards' }
     }));
 }
+
+
+
+
+
+
+
+
+
+
+
+// flashcardController.postCard: (req, res, next) => {
+//   const { message, password } = req.body;
+//   const query = 'INSERT INTO Message (message, password) VALUES ($1, $2) RETURNING *;';
+//   const params = [message, password];
+//   // const { message } = req.body;
+//   // const query = 'INSERT INTO Message (message) VALUES ($1) RETURNING *;';
+//   // const params = [message];
+
+//   try {
+//     const response = await db.query(query, params);
+//     res.locals.newMessage = response.rows[0];
+//     return next();
+//   }
+//   catch (error) {
+//     return next({
+//       log: 'Error in messageController.postMessage',
+//       status: 500,
+//       message: { err: `An error occured in messageController.postMessage ${error}` },
+//     });
+//   }
+// },
+
+
 
 module.exports = flashcardController;
