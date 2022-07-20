cbae7df450cd55ff3be64bd09c16215276f1ff96
Liz <Liz5112@users.noreply.github.com>
2022-07-19 22:39:59 -0500

    Modularize Flashcard Controller

--- server/flashcardController.js
+++ server/flashcardController.js
@@ -3,20 +3,20 @@ const db = require('./FlashcardModel.js');
 const flashcardController = {};
 
 // get a list of all the decks (tables) in the database to use for the drop down menu
-flashcardController.getDecks = (req, res, next) => {
+// flashcardController.getDecks = (req, res, next) => {
 
-  const queryString = `SELECT * FROM information_schema.tables WHERE table_schema = 'public'  AND table_name != 'pg_stat_statements';`;
+//   const queryString = `SELECT * FROM information_schema.tables WHERE table_schema = 'public'  AND table_name != 'pg_stat_statements';`;
 
-  db.query(queryString)
-    .then((data) => {
-      res.locals.decks = data.rows;
-      return next();
-    })
-    .catch((err) => next({
-      log: `Error in flashcardController.getDecks: ${err}`,
-      message: { err: 'Error getting decks' }
-    }));
-}
+//   db.query(queryString)
+//     .then((data) => {
+//       res.locals.decks = data.rows;
+//       return next();
+//     })
+//     .catch((err) => next({
+//       log: `Error in flashcardController.getDecks: ${err}`,
+//       message: { err: 'Error getting decks' }
+//     }));
+// }
 
 // send all cards of the specified deck from the database to the front end
 flashcardController.getCards = (req, res, next) => {
@@ -34,48 +34,24 @@ flashcardController.getCards = (req, res, next) => {
     }));
 }
 
-// add a deck to the database
-flashcardController.addDeck = (req, res, next) => {
-  console.log('req.params:', req.params)
-  const { deckName } = req.params;
-  console.log('deckName:', deckName)
-  const queryString = `CREATE TABLE ${deckName} (
-    CardId serial NOT NULL PRIMARY KEY,
-    Front varchar(255),
-    Back varchar(255)
-);`;
-
-  db.query(queryString)
-    .then((data) => {
-      // need to doublecheck this because I'm not sure if adding a deck returns anything
-      // res.locals.added = data.rows[0];
-      res.locals.added = `You have successfully added ${deckName}`;
-      return next();
-    })
-    .catch((err) => next({
-      log: `Error in flashcardController.addDeck: ${err}`,
-      message: { err: 'Error adding deck' }
-    }));
-}
-
-// delete a deck fron the database
-flashcardController.deleteDeck = (req, res, next) => {
+// add a card to the current deck in the database
+flashcardController.addCard = (req, res, next) => {
   const { currentDeck } = req.params;
-  const queryString = `DROP TABLE ${currentDeck};`;
+  const { front, back } = req.params.message;
+  const params = [front, back]
+  // TODO: need to figure out how to account for decks with titles longer than one word
+  // const queryString = `CREATE TABLE '${deckName}' (
+  const queryString = `INSERT INTO ${currentDeck} (front, back) VALUES ($1, $2);`;
 
-  db.query(queryString)
+  db.query(queryString, params)
     .then((data) => {
-      // need to doublecheck this because I'm not sure if deleting a deck returns anything
-      // res.locals.deleted = data.rows[0];
-      res.locals.deleted = `You have successfully deleted ${currentDeck}`;
+      res.locals.added = `You have successfully added a new card to ${currentDeck}`;
       return next();
     })
     .catch((err) => next({
-      log: `Error in flashcardController.deleteDeck: ${err}`,
-      message: { err: 'Error deleting deck' }
+      log: `Error in flashcardController.addCard: ${err}`,
+      message: { err: 'Error adding flashcard' }
     }));
 }
-
-
 
 module.exports = flashcardController;
