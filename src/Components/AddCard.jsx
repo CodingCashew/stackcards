de7e7d24f325b9935d5060442bf4fe461355b1d4
Liz <Liz5112@users.noreply.github.com>
2022-07-19 22:41:33 -0500

    Render Input Forms when user clicks add card icon

--- src/Components/AddCard.jsx
+++ src/Components/AddCard.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import { React, useState } from 'react';
+import { FormControl, FormLabel, Input, Button } from '@chakra-ui/react';
+import './CardContainer';
+import './DeckMenu'
 
-function AddCard() {
+function AddCard({deckName, decks, index, setAddCard, handleAddCard, currentCard, currentDeck}) {
+  // const currentDeck = decks[index];
+  const [cardFront, setCardFront] = useState('');
+  const [cardBack, setCardBack] = useState('');
+
+  const handleSetCardFront = (e) => setCardFront(e.target.value)
+  const handleSetCardBack = (e) => setCardBack(e.target.value)
+
+  // const addCardToDb = () => {console.log('adding card to db...')}
+  const addCardToDb = async () => {
+    if (cardFront && cardBack) {
+      fetch(`/addDeck/${currentDeck}`, {
+        method: 'POST',
+        body: JSON.stringify({ front: cardFront, back: cardBack })
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data)
+        })
+        .catch((err) => console.log(err));
+    }
+    setAddCard(false);
+  }
+
+  const handleCancel = () => {
+    setAddCard(false);
+  }
+
   return (
-    <div>AddCard</div>
+    <FormControl gridGap={4}>
+      <FormLabel>Enter Card Front:</FormLabel>
+      <Input placeholder='Front of Card' value={cardBack} onChange={handleSetCardFront} />
+      <FormLabel>Enter Card Back:</FormLabel>
+      <Input placeholder='Back of Card' value={cardFront} onChange={handleSetCardBack} />
+      <Button color="white" bgColor="pink1" mt={5} type="submit" onClick={addCardToDb}>Add Card</Button>
+      <Button mt={5} ml={2} onClick={handleCancel}>Cancel</Button>
+    </FormControl>
   )
 }
 
