365fb958c206d5dff3fc3a236c6159a54fab30d4
Liz <Liz5112@users.noreply.github.com>
2022-07-25 23:10:27 -0500

    Fix add and delete flashcard middleware

--- server/flashcardController.js
+++ server/flashcardController.js
@@ -21,32 +21,30 @@ flashcardController.getCards = (req, res, next) => {
 // add a card to the current deck in the database
 flashcardController.addCard = (req, res, next) => {
   const { currentDeck } = req.params;
-  console.log('currentDeck:', currentDeck)
-  const { front, back } = req.body;
-  console.log('front:', front, 'back:', back)
+  const { front, back } = req.body.values;
   const params = [front, back]
   // TODO: need to figure out how to account for decks with titles longer than one word
   // const queryString = `CREATE TABLE '${deckName}' (
   const queryString = `INSERT INTO ${currentDeck} (front, back) VALUES ($1, $2);`;
 
   db.query(queryString, params)
-    .then((data) => {
-      res.locals.added = `You have successfully added front: ${front} and back: ${back} to ${currentDeck}`;
-      return next();
-    })
-    .catch((err) => next({
-      log: `Error in flashcardController.addCard: ${err}`,
-      message: { err: 'Error adding flashcard' }
-    }));
+  .then((data) => {
+    res.locals.added = `You have successfully added front: ${front} and back: ${back} to ${currentDeck}`;
+    return next();
+  })
+  .catch((err) => next({
+    log: `Error in flashcardController.addCard: ${err}`,
+    message: { err: 'Error adding flashcard' }
+  }));
 }
 
 flashcardController.deleteCard = (req, res, next) => {
-  const { currentDeck, currentCard } = req.params;
-  const queryString = `DELETE ${currentCard} from ${currentDeck};`;
-
+  const { currentDeck } = req.params;
+  const { cardid } = req.body;
+  const queryString = `DELETE FROM ${currentDeck} where cardid = ${cardid};`;
   db.query(queryString)
     .then((data) => {
-      res.locals.deleted = `You have successfully deleted ${currentCard} from ${currentDeck}.`;
+      res.locals.deleted = `You have successfully deleted card id: "${cardid}" from ${currentDeck}.`;
       return next();
     })
     .catch((err) => next({
